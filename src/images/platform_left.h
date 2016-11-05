7937574bfd5ec711971047b8a9cb3a6ccbc96f63
yoogchu <chu.eugeneh@gmail.com>
2016-11-05 10:25:48 -0400

    platform c h files

---
+++ src/images/platform_left.h
@@ -0,0 +1,6 @@
+#ifndef PLATFORM_LEFT_BITMAP_H
+#define PLATFORM_LEFT_BITMAP_H
+extern const unsigned short platform_left[231];
+#define PLATFORM_LEFT_WIDTH 11
+#define PLATFORM_LEFT_HEIGHT 21
+#endif
